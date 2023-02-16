983aa4ae63dfcb730c1e92f1ce8be4d7337b6b21
Erieorin <ms05042004@gmail.com>
2023-02-16 10:55:31 +0300

    add hide

--- локации/Assets/scripts/enemy.cs
+++ локации/Assets/scripts/enemy.cs
@@ -2,15 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum EnemyState
-{
-    idle,
-    walk,
-    attack
-}
 public class enemy : MonoBehaviour
 {
-    public EnemyState currentstate;
     public int health;
     public string enemyname;
     public int baseAttack;
--- локации/Assets/scripts/playermovement.cs
+++ локации/Assets/scripts/playermovement.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class playermovement : MonoBehaviour
 {
-
     public float moveSpeed = 5f;
-
-    public Rigidbody2D rd;
+    public Rigidbody2D rb;
     public Animator animator;
     public vectorvalue startingPosition;
+
+    private bool canHide = false;
+    private bool hiding = false;
+    private SpriteRenderer rend;
 
     Vector2 movement;
     void Start()
     {
         transform.position = startingPosition.initialValue;
-
+        rend = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -26,11 +28,43 @@ public class playermovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("speed", movement.sqrMagnitude);
+
+        if (canHide && Input.GetKey("q"))
+        {
+            Physics2D.IgnoreLayerCollision(8, 9, true);
+            rend.sortingOrder = -1;
+            hiding = true;
+        }
+        else
+        {
+            Physics2D.IgnoreLayerCollision(8, 9, false);
+            rend.sortingOrder = 2;
+            hiding = false;
+        }
     }
 
 
     void FixedUpdate()
     {
-        rd.MovePosition(rd.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (!hiding)
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        else
+            rb.velocity = Vector2.zero;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name.Equals("hide"))
+        {
+            canHide = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name.Equals("hide"))
+        {
+            canHide = false;
+        }
     }
 }
